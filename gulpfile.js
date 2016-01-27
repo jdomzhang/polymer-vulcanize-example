dc1bc1c3bd3d923a6623f3463c56026cfd563c8d
Steven Zhang <jdomzhang@gmail.com>
2016-01-27 14:53:38 +0800

    building sample folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,42 @@
 var gulp = require('gulp');
+var serve = require('gulp-serve');
+var vulcanize = require('gulp-vulcanize');
 
-gulp.task('generate', function(){
-
+gulp.task('vulcanize', ['vulcanize-dashboard-1', 'vulcanize-index', 'vulcanize-elements'], function(){
+	
 })
+
+gulp.task('vulcanize-layouts', function(){
+	// no vulcanize, just copy all folders/files under layouts
+	return gulp.src('app/layouts/dashboards/')
+	.pipe(vulcanize({
+		excludes:['app/elements/index.html',
+			'app/components/*/*.*',
+		]
+	}))
+	.pipe(gulp.dest('dist/layouts/'))
+})
+
+// gulp.task('vulcanize-dashboard-1', function(){
+// 	return gulp.src('app/layouts/dashboards/dashboard-1.html')
+// 	.pipe(vulcanize({
+// 		abspath: '',
+// 		excludes: ['app/elements/index.html']
+// 	}))
+// 	.pipe(gulp.dest('dist/layouts/dashboards/'))
+// })
+
+gulp.task('vulcanize-index', function(){
+	// no vulcanize for root index.html
+	return gulp.src('app/index.html')
+	.pipe(gulp.dest('dist/'))
+})
+
+gulp.task('vulcanize-elements', function(){
+	return gulp.src('app/elements/index.html')
+	.pipe(vulcanize({
+	}))
+	.pipe(gulp.dest('dist/elements/'))
+})
+
+gulp.task('serve', serve('app'))
