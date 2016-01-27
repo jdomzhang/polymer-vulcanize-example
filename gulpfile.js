c75854aedf26134d61212f8b339dce7c854667d6
Steven Zhang <jdomzhang@gmail.com>
2016-01-27 16:08:08 +0800

    reorganize to layouts->elements->components

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,55 @@ var gulp = require('gulp');
 var serve = require('gulp-serve');
 var vulcanize = require('gulp-vulcanize');
 
-gulp.task('vulcanize', ['vulcanize-dashboard-1', 'vulcanize-index', 'vulcanize-elements'], function(){
-	
+gulp.task('vulcanize', ['vulcanize-index'
+	, 'vulcanize-components', 'vulcanize-elements', 'vulcanize-layouts'
+	, 'vulcanize-copy-webcomponent'
+	, 'vulcanize-copy-polymer'
+	]
+	, function(){
 })
 
 gulp.task('vulcanize-layouts', function(){
 	// no vulcanize, just copy all folders/files under layouts
-	return gulp.src('app/layouts/dashboards/')
-	.pipe(vulcanize({
-		excludes:['app/elements/index.html',
-			'app/components/*/*.*',
-		]
-	}))
-	.pipe(gulp.dest('dist/layouts/'))
+	return gulp.src('app/layouts/**/*.*')
+	.pipe(gulp.dest('dist/layouts'))
 })
 
-// gulp.task('vulcanize-dashboard-1', function(){
-// 	return gulp.src('app/layouts/dashboards/dashboard-1.html')
-// 	.pipe(vulcanize({
-// 		abspath: '',
-// 		excludes: ['app/elements/index.html']
-// 	}))
-// 	.pipe(gulp.dest('dist/layouts/dashboards/'))
-// })
 
 gulp.task('vulcanize-index', function(){
 	// no vulcanize for root index.html
 	return gulp.src('app/index.html')
+	// .pipe(vulcanize({
+	// 	excludes: ['app/layouts/index.html']
+	// }))
 	.pipe(gulp.dest('dist/'))
 })
 
+gulp.task('vulcanize-copy-webcomponent', function(){
+	return gulp.src('app/bower_components/webcomponentsjs/webcomponents-lite.min.js')
+		.pipe(gulp.dest('dist/bower_components/webcomponentsjs/'))
+})
+
+gulp.task('vulcanize-copy-polymer', function(){
+	return gulp.src('app/bower_components/polymer/*.html')
+		.pipe(gulp.dest('dist/bower_components/polymer/'))
+})
+
 gulp.task('vulcanize-elements', function(){
-	return gulp.src('app/elements/index.html')
+	return gulp.src('app/elements/**/index.html')
 	.pipe(vulcanize({
+		excludes: ['app/components/index.html']
 	}))
 	.pipe(gulp.dest('dist/elements/'))
 })
 
+gulp.task('vulcanize-components', function(){
+	return gulp.src('app/components/index.html')
+	.pipe(vulcanize({
+	}))
+	.pipe(gulp.dest('dist/components/'))
+})
+
 gulp.task('serve', serve('app'))
+
+gulp.task('serve:dist', serve('dist'))
